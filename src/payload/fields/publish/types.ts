6a845c6fa63526e4091fa81cf165456dd7c86ee2
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-09-26 14:48:43 +0530

    chore: deleted old schedule plugin

--- src/payload/fields/publish/types.ts
+++
@@ -1,9 +0,0 @@
-import { CollectionSlug } from "payload";
-
-type Position = "sidebar" | "start" | "end";
-
-export interface PluginTypes {
-  enabled?: boolean;
-  collections: CollectionSlug[];
-  position?: Position;
-}
