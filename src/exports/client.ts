75f7acb006741f1aa6a4bc4039a87a5cf00c199b
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-09-26 14:51:58 +0530

    chore: exporting custom components

--- src/exports/client.ts
+++ src/exports/client.ts
@@ -1,3 +1,4 @@
-export { CustomPathField } from "../payload/fields/path/components/CustomPathField.js";
-export { CustomSlugField } from "../payload/fields/slug/components/CustomSlugField.js";
-export { CustomPublishOnField } from "../payload/fields/publish/components/CustomPublishOnField.js";
+export { CustomPathField } from '../payload/fields/path/components/CustomPathField.js'
+export { CustomSlugField } from '../payload/fields/slug/components/CustomSlugField.js'
+export { CustomPublishOnField } from '../payload/plugins/schedule-doc-publish-plugin/fields/publishOn/components/CustomPublishOnField.js'
+export { CustomPublishOnFieldLabel } from '../payload/plugins/schedule-doc-publish-plugin/fields/publishOn/components/CustomPublishOnFieldLabel.js'
