e1ca9b08254bd2347cf72743738977e600002cb3
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-09-26 14:49:50 +0530

    fix: validate path

--- src/payload/fields/path/hooks/generateAndValidatePath.ts
+++ src/payload/fields/path/hooks/generateAndValidatePath.ts
@@ -1,9 +1,9 @@
-import { willPathConflict } from "../utils/willPathConflict.js";
-import { getParents } from "@payloadcms/plugin-nested-docs";
-import { APIError, type FieldHook } from "payload";
+import { getParents } from '@payloadcms/plugin-nested-docs'
+import { APIError, type FieldHook } from 'payload'
+import { willPathConflict } from '../utils/willPathConflict.js'
 
-import { generateBreadcrumbsUrl } from "../../../../utils/generateBreadcrumbsUrl.js";
-import { collectionSlug } from "../../../../core/collectionSlug.js";
+import { collectionSlug } from '../../../../core/collectionSlug.js'
+import { generateBreadcrumbsUrl } from '../../../../utils/generateBreadcrumbsUrl.js'
 
 export const generateAndValidatePath: FieldHook = async ({
   collection,
@@ -15,55 +15,57 @@ export const generateAndValidatePath: FieldHook = async ({
 }) => {
   // This can happen if auto save is on.
   if (
-    operation === "create" &&
+    operation === 'create' &&
     value == null &&
     (Object.keys(originalDoc).length === 0 ||
       Object.keys(siblingData).length === 0)
   )
-    return value;
+    return value
 
-  const { payload } = req;
+  const { payload } = req
 
-  if (!payload) return value; // If not server side exist
+  if (!payload) return value // If not server side exist
 
-  const currentDoc = { ...originalDoc, ...siblingData };
+  const currentDoc = { ...originalDoc, ...siblingData }
 
-  if (siblingData?.pathMode && siblingData?.pathMode === "custom") {
-    return value;
+  if (siblingData?.pathMode && siblingData?.pathMode === 'custom') {
+    return value
   }
 
   const docs = await getParents(
     req,
     // @ts-ignore
-    { parentFieldSlug: "parent" },
+    { parentFieldSlug: 'parent' },
     collection,
     currentDoc,
-    [currentDoc]
-  );
+    [currentDoc],
+  )
 
-  const updatedPath = generateBreadcrumbsUrl(docs, currentDoc);
+  const updatedPath = generateBreadcrumbsUrl(docs, currentDoc)
   const isNewPathConflicting = await willPathConflict({
     payload,
     path: updatedPath,
     currentDocId: currentDoc.id,
     currentCollection: collection ? collection.slug : collectionSlug.pages,
     collectionsToCheck: [collectionSlug.pages], // Add more collections as needed
-  });
+  })
 
   if (isNewPathConflicting) {
     const error = new APIError(
-      "This will create a conflict with an existing path.",
+      updatedPath === '/'
+        ? 'A home page already exists.'
+        : 'This will create a conflict with an existing path.',
       400,
       [
         {
-          field: "path",
-          message: "This will create a conflict with an existing path.",
+          field: 'path',
+          message: 'This will conflict with an existing path.',
         },
       ],
-      false
-    );
-    throw error;
+      false,
+    )
+    throw error
   }
 
-  return updatedPath;
-};
+  return updatedPath
+}
