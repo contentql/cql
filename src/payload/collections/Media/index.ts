4fb60e062d00764eff8aaad734c50d2b05d9ea74
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-10-03 21:07:31 +0530

    chore: media access

--- src/payload/collections/Media/index.ts
+++ src/payload/collections/Media/index.ts
@@ -1,42 +1,42 @@
-import { collectionSlug } from "../../../core/collectionSlug.js";
-import { isAdminOrCurrentUser, isAdminOrAuthor } from "../../access/index.js";
+import { collectionSlug } from '../../../core/collectionSlug.js'
 import {
   CustomCollectionConfig,
   CustomField,
-} from "../../../core/payload-overrides.js";
+} from '../../../core/payload-overrides.js'
+import { isAdminOrAuthor } from '../../access/isAdminOrAuthor.js'
 
 const urlField: CustomField = {
-  name: "url",
-  type: "text",
-};
+  name: 'url',
+  type: 'text',
+}
 
 export const Media: CustomCollectionConfig = {
   slug: collectionSlug.media,
   access: {
     read: () => true,
-    update: isAdminOrCurrentUser,
-    delete: isAdminOrCurrentUser,
+    update: isAdminOrAuthor,
+    delete: isAdminOrAuthor,
     create: isAdminOrAuthor,
   },
   upload: {
     imageSizes: [
       {
-        name: "thumbnail",
+        name: 'thumbnail',
         width: 400,
         height: 300,
-        position: "centre",
+        position: 'centre',
       },
       {
-        name: "blogImageSize2",
+        name: 'blogImageSize2',
         width: 986,
         height: undefined,
-        position: "centre",
+        position: 'centre',
       },
       {
-        name: "blogImageSize3",
+        name: 'blogImageSize3',
         width: 1470,
         height: undefined,
-        position: "centre",
+        position: 'centre',
       },
     ],
     focalPoint: false,
@@ -44,22 +44,22 @@ export const Media: CustomCollectionConfig = {
   },
   fields: [
     {
-      name: "alt",
-      label: "Alt Text",
-      type: "text",
+      name: 'alt',
+      label: 'Alt Text',
+      type: 'text',
     },
     // The following fields should be able to be merged in to default upload fields
     urlField,
     {
-      name: "sizes",
-      type: "group",
+      name: 'sizes',
+      type: 'group',
       fields: [
         {
-          name: "square",
-          type: "group",
+          name: 'square',
+          type: 'group',
           fields: [urlField],
         },
       ],
     },
   ],
-};
+}
