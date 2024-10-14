2706461c1a46abb48dae28239f6b9f3a8a820509
julydeath <manojkarajada.mk@gmail.com>
2024-10-14 18:33:27 +0530

    chore: beforeHook to create hook to create products in stripe

---
+++ src/payload/plugins/stripe-membership-plugin/collections/SubscriptionPlans/hooks/beforeCreatePlanCreateInStripe.ts
@@ -0,0 +1,45 @@
+import { CollectionBeforeChangeHook } from 'payload'
+import Stripe from 'stripe'
+
+//! Get stripe token
+const stripeSdk = new Stripe('STRIPE_SECRET_KEY')
+
+export const handleStripeProductAndPrice: CollectionBeforeChangeHook = async ({
+  operation,
+  data,
+  originalDoc,
+}) => {
+  if (operation === 'create') {
+    const product = await stripeSdk.products.create({
+      name: data.name,
+    })
+    const price = await stripeSdk.prices.create({
+      product: product.id,
+      unit_amount: data.price * 100,
+      currency: 'usd',
+      recurring: { interval: 'month' },
+    })
+
+    data.stripeProductId = product.id
+    data.stripePriceId = price.id
+  } else if (operation === 'update') {
+    // Check if name has changed
+    if (data.name !== originalDoc.name) {
+      await stripeSdk.products.update(data.stripeProductId, {
+        name: data.name,
+      })
+    }
+
+    // Check if price has changed
+    if (data.price !== originalDoc.price) {
+      const newPrice = await stripeSdk.prices.create({
+        product: data.stripeProductId,
+        unit_amount: data.price * 100,
+        currency: 'usd',
+        recurring: { interval: 'month' },
+      })
+      data.stripePriceId = newPrice.id
+    }
+  }
+  return data
+}
